c4cc172b5eeb4afd6139b50865d2e0591a6f1895
Edicius <150638513+Edicius-Lia@users.noreply.github.com>
2026-03-12 13:51:29 +0800

    build

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WFPTabControl.ViewModels
+{
+
+    public partial class MainViewModel : ObservableObject
+    {
+        // 将字段声明为可为 null
+        private TabItemViewModel? _draggedItem;
+     
+        [ObservableProperty]
+        private ObservableCollection<TabItemViewModel> _tabs = [];
+
+        [ObservableProperty]
+        private TabItemViewModel? _selectedTab;
+
+
+        [RelayCommand]
+        private void NewTab(string header)
+        {
+            var existing = Tabs.FirstOrDefault(t => t.Header == header);
+            if (existing != null)
+            {
+                SelectedTab = existing;
+                return;
+            }
+
+            var newTab = new TabItemViewModel { Header = header };
+            Tabs.Add(newTab);
+            SelectedTab = newTab;
+        }
+
+        [RelayCommand]
+        private void CloseTab(TabItemViewModel tab)
+        {
+            Tabs.Remove(tab);
+            if (SelectedTab == tab)
+                SelectedTab = Tabs.LastOrDefault();
+        }
+    }
+}
---
+++ ViewModels/TabItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFPTabControl.ViewModels
+{
+    public partial class TabItemViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _header = "New Tab";
+    }
+}
