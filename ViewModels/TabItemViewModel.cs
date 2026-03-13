983a7e8afce00e0c552978e239495e36a5cbfdcd
Edicius <150638513+Edicius-Lia@users.noreply.github.com>
2026-03-13 14:42:45 +0800

    Enhance TabItemViewModel with content and close command

--- ViewModels/TabItemViewModel.cs
+++ ViewModels/TabItemViewModel.cs
@@ -1,15 +1,39 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WFPTabControl.ViewModels
 {
     public partial class TabItemViewModel : ObservableObject
     {
         [ObservableProperty]
-        private string _header = "New Tab";
+        private string _Header = "New Tab";
+
+        [ObservableProperty]
+        private object _Content = new object();
+
+        public TabItemViewModel(string header,Frame frame )
+        {
+            Header = header;
+            Content = frame;
+        }
+        public TabItemViewModel() { }
+        [RelayCommand]
+        private void Close()
+        {
+            var parentViewModel = App.Current.Services.GetService<MainViewModel>();
+            if (parentViewModel != null)
+            {
+                parentViewModel.Tabs.Remove(this);
+                MainViewModel.ClearMemory();
+            }
+        }
     }
 }
